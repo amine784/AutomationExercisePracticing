e0d0a22cfbbf7686d173ba8f7703fb6e767f104d
ahmed__mohamed <ahmedmohamed660000@gmail.com>
2024-03-24 15:20:21 +0200

    last pr updated Done

--- src/main/java/com/automationexercise/Pages/P02_SearchProductPage.java
+++ src/main/java/com/automationexercise/Pages/P02_SearchProductPage.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 import static com.automationexercise.DriverManager.DriverManager.getDriver;
-import static com.automationexercise.Utilities.Utility.clicking;
-import static com.automationexercise.Utilities.Utility.sendData;
+import static com.automationexercise.Utilities.Utility.*;
 import static com.automationexercise.Utilities.WaitsUtils.explicitlyWaitForVisibility;
 
 public class P02_SearchProductPage {
@@ -24,8 +23,8 @@ public class P02_SearchProductPage {
     By AllProductPage = By.cssSelector("div[class=\"features_items\"]>h2");
     By SearchField = By.xpath("//input[@id=\"search_product\"]");
     By SearchButton = By.cssSelector("button[id=\"submit_search\"]");
-    WebElement SearchedSection = getDriver().findElement
-            (By.xpath("//h2[@class=\"title text-center\" and contains(text(), 'Searched Products')]"));
+    By SearchedSection =
+            By.xpath("//h2[@class=\"title text-center\" and contains(text(), 'Searched Products')]");
     List<WebElement> SearchResults = getDriver().findElements
             (By.xpath("//div[@id=\"cartModal\"]//following::div[@class=\"col-sm-4\"]"));
 
@@ -48,8 +47,8 @@ public class P02_SearchProductPage {
 
 
     public Boolean VerifySearchedProductsSectionVisible() {
-        explicitlyWaitForVisibility(driver, (By) SearchedSection);
-        return SearchedSection.isDisplayed();
+        explicitlyWaitForVisibility(driver, SearchedSection);
+        return verifyElementVisible(SearchedSection);
     }
 
     public int VerifyAllProductRelatedToSearchVisible() {
